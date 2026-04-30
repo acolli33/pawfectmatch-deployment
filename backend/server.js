37281dd86dc9a207b49c139a21464ebec9535a8a
mina <hirabaym@oregonstate.edu>
2026-04-30 19:18:30 +0900

    Updated server.js

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,16 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 //import animalsRouter from './routes/animalsRoutes.js';
+import messagesRouter from './routes/messagesRoutes.js';
+import threadsRouter from './routes/threadsRoutes.js';
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/messages', messagesRouter);
+app.use('/threads', threadsRouter);
 
 // Health check
 app.get('/', (_req, res) => res.send('Backend is running'));
